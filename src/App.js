899a343bfc6a1a8507e84717054d4a34830aa71a
Kurtis Rogers <krogers90@pop-os.localdomain>
2019-06-23 15:04:33 +0100

    KR - initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Icon, Card, DatePicker, Layout, Menu, Breadcrumb } from 'antd';
+// import antd style - learn how to install with WebPack in future
+import 'antd/dist/antd.css';
 import './App.css';
+import HeaderContent from './components/Header/HeaderContent';
+import FooterContent from './components/Footer/FooterContent';
+
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Layout style={{ minHeight: '100vh' }} className="layout">
+        <Header
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center'
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <HeaderContent />
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+            Content
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          <FooterContent />
+        </Footer>
+      </Layout>
     </div>
   );
 }
