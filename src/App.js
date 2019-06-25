b471719a20b417a23a974c07e804a1108d973017
Kurtis Rogers <krogers90@pop-os.localdomain>
2019-06-25 19:54:46 +0100

    More work done on the pages - finishing the banner component off

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-
 import { Icon, Card, DatePicker, Layout, Menu, Breadcrumb } from 'antd';
 // import antd style - learn how to install with WebPack in future
 import 'antd/dist/antd.css';
 import './App.css';
 import HeaderContent from './components/Header/HeaderContent';
 import FooterContent from './components/Footer/FooterContent';
-import Routes from './routes/Routes';
+import MainContent from './components/Main/MainContent';
 
 const { Header, Content, Footer } = Layout;
 
-function App() {
-  return (
-    <div className="App">
-      <Layout style={{ minHeight: '100vh' }} className="layout">
-        <Header
-          style={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center'
-          }}
-        >
-          <HeaderContent />
-        </Header>
-        <Content style={{ padding: '0 50px' }}>
-          <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
-            Content
-          </div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>
-          <FooterContent />
-        </Footer>
-      </Layout>
-    </div>
-  );
+class App extends PureComponent {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Layout style={{ minHeight: '100vh' }} className="layout">
+            <Header
+              style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                zIndex: 1
+              }}
+            >
+              <HeaderContent />
+            </Header>
+            <MainContent />
+            <Footer style={{ textAlign: 'center' }}>
+              <FooterContent />
+            </Footer>
+          </Layout>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
